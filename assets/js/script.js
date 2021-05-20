17c6196bf404dc86815ecb708e28178efed24080
Stefan Helbick <helbickstefan@gmail.com>
2021-05-20 21:46:07 +0200

    remplissage

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,76 @@ window.onclick = function (event) {
     }
   }
 }
+jQuery(document).ready(function() {
+
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+   
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 30;
+    mouseY = e.pageY - 30; 
+  });
+    
+  setInterval(function(){
+    xp += ((mouseX - xp)/10);
+    yp += ((mouseY - yp)/10);
+    $("#circle").css({left: xp +'px', top: yp +'px'});
+  }, 20);
+
+});
+
+jQuery(document).ready(function() {
+
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+   
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 30;
+    mouseY = e.pageY - 30; 
+  });
+    
+  setInterval(function(){
+    xp += ((mouseX - xp)/20);
+    yp += ((mouseY - yp)/20);
+    $("#circle2").css({left: xp +'px', top: yp +'px'});
+  }, 20);
+
+});
+
+jQuery(document).ready(function() {
+
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+   
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 30;
+    mouseY = e.pageY - 30; 
+  });
+    
+  setInterval(function(){
+    xp += ((mouseX - xp)/30);
+    yp += ((mouseY - yp)/30);
+    $("#circle3").css({left: xp +'px', top: yp +'px'});
+  }, 20);
+
+});
+
+jQuery(document).ready(function() {
+
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+   
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 30;
+    mouseY = e.pageY - 30; 
+  });
+    
+  setInterval(function(){
+    xp += ((mouseX - xp)/40);
+    yp += ((mouseY - yp)/40);
+    $("#circle4").css({left: xp +'px', top: yp +'px'});
+  }, 20);
+
+});
+
 
